8efe4c0423d172338ae72a6e8bb4bc8c8f6c31a3
tafazulaijaz <tafazulqureshi1@gmail.com>
2026-01-20 10:30:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let userInput = document.querySelector("#userInp");
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let AttemptCount = 0;
+const guessButton = document.querySelector("#guessBtn");
+let guessNumber = document.querySelector("#computerRsp");
+let Attempts = document.querySelector("#totalAttempt");
+const resetButton = document.querySelector("#resetBtn");
+debugger
+guessButton.addEventListener("click", function () {
+  AttemptCount = AttemptCount + 1;
+  Attempts.innerText = "attempts:" + AttemptCount;
+  const typedNumber = Number(userInput.value);
+     if (typedNumber < secretNumber) {
+    guessNumber.innerText = "wrong to Low " + typedNumber;
+    guessNumber.style.color = "red";
+  } else if (typedNumber > secretNumber) {
+    guessNumber.innerText = "Wrong Too high " + typedNumber;
+    guessNumber.style.color = "red";
+  } else {
+    guessNumber.innerText = "correct " + typedNumber;
+    guessNumber.style.color = "green";
+     guessButton.disabled = true;
+  }
+    if (AttemptCount >= 5 && typedNumber !== secretNumber) {
+    guessButton.disabled = true;
+  }
+});
+
+resetButton.addEventListener("click", function () {
+  AttemptCount = 0;
+  guessButton.disabled = false
+  Attempts.innerText = "Attempts:" + AttemptCount;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  userInput.value = "";
+  guessNumber.innerText = "start Guessing";
+  guessNumber.style.color = "black";
+});
